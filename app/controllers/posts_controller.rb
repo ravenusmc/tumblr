91afde95794955b7001dece2050b06dc322a9011
ravenusmc <mcuddy77@gmail.com>
2016-04-01 10:09:26 -0400

    added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,26 +22,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end 
 
-  # def edit 
-  #   @post = Post.find(params[:id])
-  # end 
+  def edit 
+    @post = Post.find(params[:id])
+  end 
 
-  # def update
-  #   @post = Post.find(params[:id])
+  def update
+    @post = Post.find(params[:id])
 
-  #   if @post.update(params[:post].permit(:title, :body))
-  #     redirect_to @post
-  #   else 
-  #     render 'edit'
-  #   end 
-  # end 
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else 
+      render 'edit'
+    end 
+  end 
 
-  # def destroy
-  #   @post = Post.find(params[:id])
-  #   @post.destroy
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
 
-  #   redirect_to root_path
-  # end 
+    redirect_to root_path
+  end 
 
   private
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  #dependent makes it so that when you destroy a post the comments go as well.
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
